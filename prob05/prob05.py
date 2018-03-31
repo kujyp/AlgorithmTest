fd14926116f95a8ce327630effd6c86ebc262a52
kujyp <pjo901018@gmail.com>
2018-03-31 12:26:13 +0900

    p06

--- prob05/prob05.py
+++ prob05/prob05.py
@@ -1,10 +1,47 @@
 # -*- coding: utf-8 -*-
-import sys
+# import sys
 
 
-def get_answer(albas):
+WEIGHT_IDX = 0
+HAVE_DID_ALBAS_IDX = 1
+MAXIMUM_M = 200
 
-    return ""
+START_DAY_IDX = 0
+DURATION_IDX = 1
+
+
+def get_suboptimal_before_day(sub_optimals, day):
+    if day == 0:
+        return [0, []]
+
+    return sub_optimals[day - 1]
+
+
+def get_weight_before_day(sub_optimals, day):
+    return get_suboptimal_before_day(sub_optimals, day)[WEIGHT_IDX]
+
+
+def get_answer(albas, last_day):
+    sub_optimals = [(0, [])] * MAXIMUM_M
+
+    for day in range(1, last_day + 1):
+        if day != 1:
+            sub_optimals[day] = sub_optimals[day - 1]
+
+        if len(albas[day]) == 0:
+            continue
+
+        for each_alba in albas[day]:
+            start_day, _, weight = each_alba
+
+            before_weight, before_albas = get_suboptimal_before_day(sub_optimals, start_day)
+            curr_weight = weight + before_weight
+            if curr_weight > get_weight_before_day(sub_optimals, day):
+                curr_albas = before_albas.copy()
+                curr_albas.append(each_alba)
+                sub_optimals[day] = (curr_weight, curr_albas)
+
+    return sub_optimals[last_day]
 
 
 # Time complexity: TODO
@@ -12,15 +49,23 @@ def get_answer(albas):
 def main():
     total = int(input())
     # total = int(sys.stdin.readline().strip())
-    albas = []
+    albas = [[] for _ in range(MAXIMUM_M)]
+
+    last_day = 0
     for i in range(total):
         # line = sys.stdin.readline().strip()
-        M, D, I = [int(word) for word in input().split()]
-        albas.append((M, D, I))
+        # start_day, duration, weight = [int(word) for word in line.split()]
+        start_day, duration, weight = [int(word) for word in input().split()]
+        end_day = start_day + duration - 1
+        albas[end_day].append((start_day, duration, weight))
 
-    answer = get_answer(albas)
+        last_day = max(end_day, last_day)
 
-    print(answer)
+    answer = get_answer(albas, last_day)
+
+    print(answer[WEIGHT_IDX])
+    for alba in answer[HAVE_DID_ALBAS_IDX]:
+        print("{} {}".format(alba[START_DAY_IDX], alba[DURATION_IDX]))
 
 
 if __name__ == "__main__":
--- prob05/test_prob05.py
+++ prob05/test_prob05.py
@@ -31,6 +31,56 @@ class End2EndTestCase(unittest.TestCase):
 3 4
 8 5""", output)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_end2end_2(self, captured_output):
+        raw_input = """\
+8
+1 6 10
+2 4 10
+6 3 5
+7 4 9
+9 4 11
+10 6 7
+11 6 9
+13 6 4
+"""
+        user_input = raw_input.split("\n")
+
+        with patch('builtins.input', side_effect=user_input):
+            main()
+            output = captured_output.getvalue()
+            output = output.rstrip()
+
+        self.assertEqual("""\
+30
+2 4
+6 3
+9 4
+13 6""", output)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_end2end_3(self, captured_output):
+        raw_input = """\
+6
+0 99 30
+75 46 29
+80 20 28
+99 62 29
+120 11 31
+130 51 29
+"""
+        user_input = raw_input.split("\n")
+
+        with patch('builtins.input', side_effect=user_input):
+            main()
+            output = captured_output.getvalue()
+            output = output.rstrip()
+
+        self.assertEqual("""\
+61
+0 99
+120 11""", output)
+
 #     @patch('sys.stdout', new_callable=io.StringIO)
 #     def test_end2end_2(self, captured_output):
 #         raw_input = """\
